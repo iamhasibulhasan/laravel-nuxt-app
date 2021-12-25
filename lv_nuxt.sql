1d3d01f1d6f1f69146e3d47edc73f6c9d3808006
imhasibulhasan <hashibul218@gmail.com>
2021-12-25 14:01:50 +0600

    update sql file

--- lv_nuxt.sql
+++ lv_nuxt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 15, 2021 at 06:35 PM
+-- Generation Time: Dec 25, 2021 at 09:00 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -20,6 +20,59 @@ SET time_zone = "+00:00";
 --
 -- Database: `lv_nuxt`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brands`
+--
+
+CREATE TABLE `brands` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `brands`
+--
+
+INSERT INTO `brands` (`id`, `name`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'Vaseline', 1, '2021-12-23 21:59:56', '2021-12-23 22:18:46'),
+(2, 'Nivea', 1, '2021-12-23 22:21:21', '2021-12-23 22:21:21'),
+(4, 'Coca Cola', 1, '2021-12-23 22:21:51', '2021-12-23 22:21:51'),
+(5, 'Samsung', 1, '2021-12-24 09:39:52', '2021-12-24 09:39:52'),
+(6, 'Nokia', 1, '2021-12-24 09:39:58', '2021-12-24 09:39:58'),
+(7, 'Close-up', 1, '2021-12-24 09:40:11', '2021-12-24 09:40:11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `parent` varchar(11) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `parent`, `status`, `created_at`, `updated_at`) VALUES
+(18, 'Women\'s', NULL, 1, '2021-12-23 06:58:33', '2021-12-23 07:15:50'),
+(19, 'Electronics', '17', 1, '2021-12-23 06:58:40', '2021-12-23 07:18:09'),
+(20, 'Saree', '18', 1, '2021-12-23 06:59:19', '2021-12-23 06:59:19'),
+(21, 'Lehenga', '18', 1, '2021-12-23 06:59:49', '2021-12-23 06:59:49'),
+(22, 'Panjabi', '23', 1, '2021-12-23 07:00:02', '2021-12-23 07:44:32'),
+(23, 'Men', NULL, 1, '2021-12-23 07:44:13', '2021-12-23 07:44:13');
 
 -- --------------------------------------------------------
 
@@ -54,10 +107,15 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2021_12_20_163953_create_roles_table', 2),
+(7, '2014_10_12_000000_create_users_table', 3),
+(9, '2021_12_21_155806_create_categories_table', 4),
+(11, '2021_12_24_031351_create_brands_table', 5),
+(13, '2021_12_24_042304_create_tags_table', 6),
+(16, '2021_12_23_134857_create_products_table', 7);
 
 -- --------------------------------------------------------
 
@@ -92,6 +150,80 @@ CREATE TABLE `personal_access_tokens` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `category` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `brand` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `tag` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `info` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `user_id`, `description`, `category`, `brand`, `tag`, `info`, `status`, `image`, `created_at`, `updated_at`) VALUES
+(7, 'Close up Toothpaste', '1', '<p>Feel the air.</p>', '[\"20\",\"21\"]', '[\"1\",\"2\"]', '[\"1\",\"3\"]', '{\"sku\":\"SDF\",\"regular_price\":\"48\",\"sale_price\":\"56\",\"stock_quantity\":\"55\"}', 1, 'media/product/49059505a0e9a07e2e0138123b0d556d.jpg', '2021-12-24 06:48:14', '2021-12-24 09:35:49'),
+(8, 'Vaseline Intensive Care Deep Restore Body Lotion', '1', '<p>we care for you.</p>', '[\"18\"]', '[\"1\"]', '[\"1\",\"3\",\"5\"]', '{\"sku\":\"SDF\",\"regular_price\":\"12\",\"sale_price\":\"12\",\"stock_quantity\":\"55\"}', 1, 'media/product/b32172ba7a721ca69d10f5eae2b11170.jpg', '2021-12-24 09:35:40', '2021-12-24 09:35:40'),
+(9, 'Pepsodant Toothpast', '1', '<p>Teeth glowing</p>', '[\"23\"]', '[\"1\",\"2\"]', '[\"1\",\"3\",\"5\"]', '{\"sku\":\"dsds\",\"regular_price\":\"12\",\"sale_price\":\"56\",\"stock_quantity\":\"55\"}', 1, 'media/product/114410e7a45c6c28c4068115da1b55d7.jpg', '2021-12-24 09:37:02', '2021-12-24 09:37:02'),
+(10, 'Vaseline Intensive Care Deep Restore Body Lotion', '1', '<p>fdfsf</p>', '[\"18\",\"19\"]', '[\"2\",\"4\"]', '[\"3\",\"5\",\"6\"]', '{\"sku\":\"dsdsds\",\"regular_price\":\"12\",\"sale_price\":\"12\",\"stock_quantity\":\"55\"}', 0, 'media/product/0ed79293bf5a252ffc0b307dbafc3d52.png', '2021-12-25 01:12:28', '2021-12-25 01:49:11'),
+(11, 'Breakfast 1', '1', '<p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>', '[\"18\",\"19\",\"20\"]', '[\"2\",\"4\",\"6\"]', '[\"5\",\"6\",\"7\"]', '{\"sku\":\"DSFEE\",\"regular_price\":\"12\",\"sale_price\":\"12\",\"stock_quantity\":\"55\"}', 1, 'media/product/aba294561906794e7268841856eac325.png', '2021-12-25 01:54:23', '2021-12-25 01:54:23');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `permission` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE `tags` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `tags`
+--
+
+INSERT INTO `tags` (`id`, `name`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'Shirt', 1, '2021-12-23 22:46:51', '2021-12-23 23:08:10'),
+(3, 'Winter sale', 1, '2021-12-23 22:47:08', '2021-12-23 23:08:31'),
+(5, '50% Off', 1, '2021-12-23 23:09:57', '2021-12-23 23:09:57'),
+(6, 'Regural', 1, '2021-12-24 09:39:06', '2021-12-24 09:39:06'),
+(7, 'Daily', 1, '2021-12-24 09:39:12', '2021-12-24 09:39:12'),
+(8, 'Toothpast', 1, '2021-12-24 09:39:18', '2021-12-24 09:39:18');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -99,8 +231,12 @@ CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone_number` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `photo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'avatar.png',
+  `status` tinyint(1) NOT NULL DEFAULT 1,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -110,15 +246,25 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Hasibul Hasan', 'mdhasibulhasan.dev@gmail.com', NULL, '$2y$10$kNbt.Ez9Vkefp8XrjivzXON3RdH5.VWYjZY793iN/Aa0rDEPtQfA6', NULL, '2021-12-15 03:14:32', '2021-12-15 03:14:32'),
-(2, 'Tithi Ghosh', 'tithi@gmail.com', NULL, '$2y$10$zbkEerBLVQ06bJAxCMhhh.2qq3.FgKdvAQw4g3qfeKzVeOWbqBtUu', NULL, '2021-12-15 09:07:34', '2021-12-15 09:07:34'),
-(5, 'Dipto Mondol', 'dipto@gmail.com', NULL, '$2y$10$8Qg2vztlMizz0CmCf9FiQODcX3ysYXniouN8/kN2lk0gIYo7spGwq', NULL, '2021-12-15 11:08:21', '2021-12-15 11:08:21'),
-(7, 'Afia Rahman', 'dola@gmail.com', NULL, '$2y$10$wZ3V/nG9M4F6JrbWUaIUc.Dw6B.lMnTc6Yf20jAA/Z8/IasntMrua', NULL, '2021-12-15 11:17:02', '2021-12-15 11:17:02');
+INSERT INTO `users` (`id`, `name`, `email`, `phone_number`, `username`, `email_verified_at`, `password`, `photo`, `status`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Hasibul Hasan', 'mdhasibulhasan.dev@gmail.com', NULL, 'mdhasibulhasan.dev@gmail.com', NULL, '$2y$10$SNj82GL5qUCNIZsEIxA5Eu1.mAy43T/hJ9vbIfAOHNmWMzDKBB7MG', 'avatar.png', 1, NULL, '2021-12-20 22:52:58', '2021-12-20 22:52:58');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `brands_name_unique` (`name`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `failed_jobs`
@@ -148,15 +294,47 @@ ALTER TABLE `personal_access_tokens`
   ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `roles_slug_unique` (`slug`);
+
+--
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_email_unique` (`email`);
+  ADD UNIQUE KEY `users_email_unique` (`email`),
+  ADD UNIQUE KEY `users_username_unique` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -168,7 +346,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -177,10 +355,28 @@ ALTER TABLE `personal_access_tokens`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
